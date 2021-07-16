ee2374ec43bdd04a86fc3c1d76e54c68790e4418
User1 <User1@DESKTOP-H4RQBFU>
2021-07-16 14:05:15 +0300

    Add project files.

---
+++ apiShahim/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using apiShahim.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace apiShahim.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext db;
+        public EmployeesController(AppDbContext _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await db.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var emp =await db.Employees.FindAsync(id);
+            return emp;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)// new Employee// new ID
+        {
+            db.Employees.Add(employee);
+          await  db.SaveChangesAsync();
+            return CreatedAtAction("GetEmployee", new { id= employee .ID},employee);
+        }
+
+    }
+}
---
+++ apiShahim/Migrations/20210711114045_test1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apiShahim.Migrations
+{
+    public partial class test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
